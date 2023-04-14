1bd49b180b428260198805a76587a21a1f67e239
LLwheaton <lwheaton89@gmail.com>
2023-04-14 20:44:49 +1000

    Send request to API and print response

--- server/server.js
+++ server/server.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const { Configuration, OpenAIApi } = require("openai");
 
+const app = express();
 app.use(cors());
 app.use(express.json());
+require('dotenv').config();
 
-app.get("/message", (req, res) => {
-    res.json({message: "Hello from the server!"});
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+const message = "Hello from server :)";
+
+app.get("/message", async (req, res) => {
+    const response = await openai.createCompletion({
+        model: "text-davinci-001",
+        prompt: "Give me a journal prompt related to the mood: sad",
+        max_tokens: 50,
+        temperature: 0,
+      });
+    // Displays the text response from above model and prompt
+    console.log(response.data.choices[0].text);
+    res.json({message: message});
 });
 
 app.listen(9000, () => {
